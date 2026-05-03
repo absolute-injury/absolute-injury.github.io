066270abc97b5d456d92d07e6261207e6621d8a2
absolute-injury <owlflaveredcarpet@gmail.com>
2026-05-03 14:34:30 +0000

    Deploying to gh-pages from @ absolute-injury/absolute-injury.github.io@efe75ec088038f8f35884e03b18bc3b317919a80 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "absolute-injury/absoluteinjury.github.io",
+    "data-repo": "absolute-injury/absolute-injury.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
