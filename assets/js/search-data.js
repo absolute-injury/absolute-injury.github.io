5a38925d91f3a7503dc966050c4cdd9c1a48ac65
absolute-injury <owlflaveredcarpet@gmail.com>
2026-05-03 15:23:29 +0000

    Deploying to gh-pages from @ absolute-injury/absolute-injury.github.io@8f1d0f75c2570e9632a209c6c802a9f728958c0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,21 +433,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%61%62%69%6E%31%31%32%35@%73%6E%75.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-rss',
@@ -461,14 +454,28 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=HA7JSkwAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/stilljaehun7788", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@owlflaveredcarpet", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/absolute-injury", "_blank");
         },
       },{
       id: 'light-theme',
